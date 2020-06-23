377f53de17b519dc99c976fb54aab36552dad2e3
Thomas Steur <tsteur@users.noreply.github.com>
2020-06-24 11:28:23 +1200

    Rename branch name

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 VERSION="$1"
 
-wget https://github.com/matomo-org/matomo-marketplace-for-wordpress/archive/master.zip
-unzip master.zip
-mv matomo-marketplace-for-wordpress-master matomo-marketplace-for-wordpress
+wget https://github.com/matomo-org/matomo-marketplace-for-wordpress/archive/live.zip
+unzip live.zip
+mv matomo-marketplace-for-wordpress-live matomo-marketplace-for-wordpress
 zip -r matomo-marketplace-for-wordpress-$VERSION.zip matomo-marketplace-for-wordpress
-rm master.zip
+rm live.zip
 rm -rf matomo-marketplace-for-wordpress
 scp -p matomo-marketplace-for-wordpress-$VERSION.zip "piwik-builds@matomo.org:/home/piwik-builds/www/builds.piwik.org/"
 scp -p matomo-marketplace-for-wordpress-$VERSION.zip "piwik-builds@matomo.org:/home/piwik-builds/www/builds.piwik.org/matomo-marketplace-for-wordpress-latest.zip"
