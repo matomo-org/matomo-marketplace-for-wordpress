fae25b60bb82b0ae86c7f841705ffe1c364c9518
diosmosis <diosmosis@users.noreply.github.com>
2025-10-16 22:59:38 -0700

    add lint and format npm scripts and run format on index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import domReady from '@wordpress/dom-ready';
 import { createRoot } from '@wordpress/element';
 
 const SettingsPage = () => {
-    return <div>Placeholder for settings page</div>;
+	return <div>Placeholder for settings page</div>;
 };
 
-domReady(() => {
-    const root = createRoot(
-        document.getElementById( 'matomo-marketplace-for-wordpress' )
-    );
+domReady( () => {
+	const root = createRoot(
+		document.getElementById( 'matomo-marketplace-for-wordpress' )
+	);
 
-    root.render( <SettingsPage /> );
-});
+	root.render( <SettingsPage /> );
+} );
