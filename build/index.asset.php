8ede699a3073d64c8c695468a41be4c7d4da16bb
diosmosis <diosmosis@users.noreply.github.com>
2026-04-20 16:10:28 -0700

    development build for debugging

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a86e12425c29682e2a35');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '134b7bc2fcfe4d74bedb');
