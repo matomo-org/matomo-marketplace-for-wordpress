6a04edb5abbec24b9adb660e44950562c4f44e5a
dizzy <diosmosis@users.noreply.github.com>
2026-04-22 04:37:28 -0700

    add release creation command to package.json (#34)
    * add release creation command to package.json
    
    * regenerate autoload for production

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,11 +6,4 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'eftec\\bladeone\\' => array($vendorDir . '/eftec/bladeone/lib'),
-    'WP_CLI\\I18n\\' => array($vendorDir . '/wp-cli/i18n-command/src'),
-    'Symfony\\Component\\Finder\\' => array($vendorDir . '/symfony/finder'),
-    'Peast\\' => array($vendorDir . '/mck89/peast/lib/Peast'),
-    'Mustangostang\\' => array($vendorDir . '/wp-cli/mustangostang-spyc/src'),
-    'Gettext\\Languages\\' => array($vendorDir . '/gettext/languages/src'),
-    'Gettext\\' => array($vendorDir . '/gettext/gettext/src'),
 );
